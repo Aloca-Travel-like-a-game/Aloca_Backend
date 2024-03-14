ababa423b5ca43b3a99ab0b7fb8f7b624cbb3965
Khanh <khanhabcdd1@gmail.com>
2024-03-14 22:58:37 +0700

    update the code of the endpoint createTrip

--- src/routers/rankingRouter.js
+++ src/routers/rankingRouter.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import checkAuthentication from "../middlewares/checkAuthentication.js";
+import { rankPerMonth, rankPerWeek, rankingUserHighest } from "../controllers/rankingController.js";
 const router = Router();
 
-router.get("/", checkAuthentication, getDataChat)
+router.get("/monthly", checkAuthentication, rankPerMonth)
+router.get("/weekly", checkAuthentication, rankPerWeek)
+router.get("/rankingUserHighest", checkAuthentication, rankingUserHighest)
 
 export default router;
