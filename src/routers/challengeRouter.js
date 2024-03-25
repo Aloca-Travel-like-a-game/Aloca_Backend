22c5e4b9fc2adb5ac53f9b8f000f8ea4a42ca95b
Khanh <khanhabcdd1@gmail.com>
2024-03-26 00:55:29 +0700

    update some code  challenge

--- src/routers/challengeRouter.js
+++ src/routers/challengeRouter.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { checkChallengeProgress } from "../controllers/challengeController.js";
-import checkAuthentication from "../middlewares/checkAuthentication.js";
 const router = Router();
 
-router.post("/checkProgress", checkAuthentication, checkChallengeProgress)
+router.post("/checkProgress", checkChallengeProgress)
 
 
 export default router;
