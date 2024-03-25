7a838cbd773b85e91058d54763a33a67edf2efb4
Khanh <khanhabcdd1@gmail.com>
2024-03-26 00:47:28 +0700

    update some code getlocation and challenge

--- src/controllers/challengeController.js
+++ src/controllers/challengeController.js
@@ -13,8 +13,7 @@ const geocoder = NodeGeocoder({
 
 const checkChallengeProgress = async (req, res) => {
     try {
-        const { lat, lng, chaId, imageURI } = req.body;
-        console.log("imageURI", imageURI);
+        const { lat, lng, chaId } = req.body;
         const challenge = await Challenge.findById(chaId);
         const locationChallenge = challenge.location;
         // const resLocationChanllenge = await geocoder.geocode(locationChallenge);
@@ -27,39 +26,18 @@ const checkChallengeProgress = async (req, res) => {
         //         { latitude: lat, longitude: lng },
         //         { latitude: langtitude, longitude: longitude }
         //     )}
-        const challengeLatitude = challenge.latitude;
-        const challengeLongitude = challenge.longitude;
-        const distance = geolib.getDistance(
-            { latitude: lat, longitude: lng },
-            { latitude: challengeLatitude, longitude: challengeLongitude }
-        )
-        if (distance > 80) {
-            return res.status(400).json({ message: "Please complete the mission at the location provided", distance })
-        }
-        if (!imageURI) {
-            return res.status(400).json({ message: "No image updoaded" })
-        }
-
-        const storageRef = ref(imageURI, `images/${chaId}_${Date.now()}`);
-        const uploadTask = uploadBytesResumable(storageRef, imageBuffer)
-        uploadTask.on("state_changed",
-            (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                console.log("Upload progres", progress);
-            },
-            (err) => {
-                console.log("err", err);
-            },
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref)
-                    .then((downloadURL) => {
-                        console.log("Image uploaded successfully. Download URL:", downloadURL);
-                    })
-                    .catch((downloadURLError) => {
-                        console.error("Error getting download URL:", downloadURLError);
-                    });
-            }
-        )
+        // const challengeLatitude = challenge.latitude;
+        // const challengeLongitude = challenge.longitude;
+        // const distance = geolib.getDistance(
+        //     { latitude: lat, longitude: lng },
+        //     { latitude: challengeLatitude, longitude: challengeLongitude }
+        // )
+        // if (distance > 80) {
+        //     return res.status(400).json({ message: "Please complete the mission at the location provided", distance })
+        // }
+        // if (!imageURI) {
+        //     return res.status(400).json({ message: "No image updoaded" })
+        // }
         return res.status(200).json({ message: "Complete the challenge", distance })
     } catch (err) {
         console.log(err);
