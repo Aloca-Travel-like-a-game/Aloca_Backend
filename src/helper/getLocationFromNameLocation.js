657f720480b1097178f41bdb2a3fde2d1124b464
Khanh <khanhabcdd1@gmail.com>
2024-03-27 11:24:07 +0700

    complete function checkChallengeProgress

--- src/helper/getLocationFromNameLocation.js
+++ src/helper/getLocationFromNameLocation.js
@@ -2,12 +2,12 @@ import axios from "axios";
 export const getLocationFromNameLocation = async (nameLocation, province) => {
     try {
         const APIurl =
-            `https://autosuggest.search.hereapi.com/v1/autosuggest?xnlp=CL_JSMv3.1.51.0&apikey=TYWNfgg1aUErbBEMMhjeeiX4uDup2tkboazOS0PY4BQ&at=52.5%2C13.4&lang=vi-VN&politicalView=VNM&limit=5&q=${nameLocation},${province}`;
+            `https://autosuggest.search.hereapi.com/v1/autosuggest?xnlp=CL_JSMv3.1.51.0&apikey=TYWNfgg1aUErbBEMMhjeeiX4uDup2tkboazOS0PY4BQ&at=52.5%2C13.4&lang=vi-VN&politicalView=VNM&limit=5&q=${nameLocation} ${province}`;
         const res = await axios.get(APIurl);
         if (res.data.items && res.data.items.length > 0) {
-            const latitude = res.data.items[1].position.lat;
-            const longitude = res.data.items[1].position.lng;
-            const location = res.data.items[1].address.label;
+            const latitude = res.data.items[0].position.lat;
+            const longitude = res.data.items[0].position.lng;
+            const location = res.data.items[0].address.label;
             return { latitude, longitude, location }
         }
         return
